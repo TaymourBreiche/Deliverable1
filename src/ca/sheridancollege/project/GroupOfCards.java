00ec4404ebc1ef6d6a4508316ad59cbd07795633
TaymourBreiche <133786296+TaymourBreiche@users.noreply.github.com>
2023-06-29 19:11:11 -0400

    Update GroupOfCards.java
    edit to names

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -12,8 +12,8 @@ import java.util.Collections;
  * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
  * once. The group of cards has a maximum size attribute which is flexible for reuse.
  *
- * @author Taymour Breiche 
- * @author Taymour Breiche May 12th
+ * @author Taymour Breiche and and Hieu Nguyen 
+ * @author Taymour Breiche and and Hieu Nguyen May 12th
  */
 public class GroupOfCards {
 
